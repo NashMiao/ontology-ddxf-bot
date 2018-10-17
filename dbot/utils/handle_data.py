2b1a3465e2c26f8c97b3fe14585aa0b0b3609c09
dingxin <wdx7266@vip.qq.com>
2018-10-17 14:07:32 +0800

    add data type handle function

--- dbot/utils/handle_data.py
+++ dbot/utils/handle_data.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import struct
+
+
+def int_to_little_bytes(value: int) -> bytes:
+    byte_package = struct.pack('<L', value)
+    return byte_package
+
+
+def crypto_str_to_bytes(s: str) -> bytes:
+    if isinstance(s, bytes):
+        return s
+    elif isinstance(s, str):
+        return s.encode('latin-1')
+    else:
+        return bytes(list(s))
+
+
+def crypto_bytes_to_str(bs: bytes) -> str:
+    return bs.decode('latin-1')
